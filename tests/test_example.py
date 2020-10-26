d91272d59918f22da673f56809391e8df646ccab
SashaJson <sashamiller666@gmail.com>
2020-10-26 23:32:42 +0200

    added new method test_new_booking_exists in class TestExample

--- tests/test_example.py
+++ tests/test_example.py
@@ -9,3 +9,12 @@ class TestExample:
         created = res.json()
         assert created.get("bookingid")
         assert created.get("booking") == data
+
+    def test_new_booking_exists(self, client):
+        data = random.random_booking()
+        res = client.vr(client.create_booking(data), [200, 201])
+        created = res.json()
+        bookingid = created.get("bookingid")
+        res = client.vr(client.get_booking(bookingid))
+        exists = res.json()
+        assert exists == data
