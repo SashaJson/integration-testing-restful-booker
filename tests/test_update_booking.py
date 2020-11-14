bf1b83c580e7c659f21161dae1d8b2f47d64e021
SashaJson <sashamiller666@gmail.com>
2020-11-14 20:07:33 +0200

    added assert for check statusCode from Server

--- tests/test_update_booking.py
+++ tests/test_update_booking.py
@@ -8,10 +8,12 @@ class TestUpdateBooking:
     def test_update_booking(self, client):
         data = random.random_booking()
         res = client.create_booking(data)
+        assert res.status_code == 200
         created = res.json()
         bookingid = created.get("bookingid")
         data2 = random.random_booking()
         res = client.update_booking(bookingid, data2)
+        assert res.status_code == 200
         updated = res.json()
         assert updated == data2
 
