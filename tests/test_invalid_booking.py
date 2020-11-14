b18075851833f805dcbb5c56fb2ebddd3e6373fe
SashaJson <sashamiller666@gmail.com>
2020-11-14 19:51:37 +0200

    added suit invalid book

--- tests/test_invalid_booking.py
+++ tests/test_invalid_booking.py
@@ -3,33 +3,7 @@ from random import randint
 from api import random
 
 
-class TestExample:
-
-    def test_create_new_booking(self, client):
-        data = random.random_booking()
-        res = client.create_booking(data)
-        created = res.json()
-        assert created.get("bookingid")
-        assert created.get("booking") == data
-
-    def test_new_booking_exists(self, client):
-        data = random.random_booking()
-        res = client.create_booking(data)
-        created = res.json()
-        bookingid = created.get("bookingid")
-        res = client.get_booking(bookingid)
-        exists = res.json()
-        assert exists == data
-
-    def test_update_booking(self, client):
-        data = random.random_booking()
-        res = client.create_booking(data)
-        created = res.json()
-        bookingid = created.get("bookingid")
-        data2 = random.random_booking()
-        res = client.update_booking(bookingid, data2)
-        updated = res.json()
-        assert updated == data2
+class TestInvalidBooking:
 
     def test_not_existing_booking(self, client):
         res = client.get_booking(randint(10000, 99999))
